5c4bfa1b700d4b057f18445c8bb16cb591c0481c
Salem Basakran <basakransalem@gmail.com>
2025-02-23 02:13:00 +0300

    Progress in Rank and partition

--- queries.py
+++ queries.py
@@ -1,8 +1,31 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+        SELECT
+            OrderID,
+            CustomerID,
+            OrderDate,
+            RANK() OVER (
+                PARTITION BY CustomerID
+                ORDER BY OrderDate
+            ) OrderRank
+            FROM Orders
+            """
+    return db.execute(query).fetchall()
 
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+        SELECT
+            Orders.OrderID,
+            Orders.CustomerID,
+            Orders.OrderDate,
+            SUM(SUM(OrderDetails.UnitPrice * OrderDetails.Quantity)) OVER(PARTITION BY Orders.CustomerID ORDER BY Orders.OrderDate) OrderCumulativeAmount
+        FROM Orders
+        JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+        GROUP BY Orders.OrderID
+        ORDER BY Orders.CustomerID
+    """
+
+    return db.execute(query).fetchall()
